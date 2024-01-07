095ea1599d1797ea25b868517da67c2b9f9ca4dc
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-01-08 06:59:03 +0700

    add header,banner, login & register form

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,33 +1,22 @@
 // // Layouts
 import HomePage from '~/pages/HomePage/Home';
-import LoginForm from '~/pages/LoginRegister/Login';
+import LoginForm from '~/components/LoginRegister/Login';
+import SignUp from '~/components/LoginRegister/Register';
+const publicRoutes = [
+  {
+    path: '/',
+    component: HomePage,
+  },
+  {
+    path: '/login',
+    component: LoginForm,
+  },
+  {
+    path: '/register',
+    component: SignUp,
+  },
+];
 
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+const privateRoutes = [];
 
-// const publicRoutes = [
-//   { path: '', component: HomePage, name: 'Home Page' },
-//   { path: '/employee', component: EmployeePage, name: 'Employee Page' },
-//   { path: '/parcel-tracking', component: ParcelTracking, name: 'Parcel Tracking' },
-// ];
-
-const Routes = () => {
-  // Define public routes accessible to all users
-  const routesForPublic = [
-    {
-      path: '/',
-      element: <HomePage />,
-    },
-    {
-      path: '/login',
-      element: <LoginForm />,
-    },
-  ];
-
-  // Combine and conditionally include routes based on authentication status
-  const router = createBrowserRouter([...routesForPublic]);
-
-  // Provide the router configuration using RouterProvider
-  return <RouterProvider router={router} />;
-};
-
-export default Routes;
+export { publicRoutes, privateRoutes };
