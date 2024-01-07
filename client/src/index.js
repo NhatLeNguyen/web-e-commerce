4386fe770fddb9130cffe6606cf369cc0cd42c84
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-01-07 20:20:14 +0700

    config linh tinh

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import GlobalStyles from './components/GlobalStyles';
+import { DevSupport } from '@react-buddy/ide-toolbox';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+   
+      <GlobalStyles>
+        <DevSupport>
+          <App />
+        </DevSupport>
+      </GlobalStyles>
+    
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ client/src/pages/HomePage/Home.js
@@ -0,0 +1,11 @@
+import Header from '~/components/header/header';
+
+const HomePage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default HomePage;
