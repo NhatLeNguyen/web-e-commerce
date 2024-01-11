f66a14947e46c853850ac29dfc7663c92e5183c3
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-01-12 02:48:48 +0700

    add + styling new arrival product

--- client/src/assets/images/index.js
+++ client/src/assets/images/index.js
@@ -3,6 +3,9 @@ const images = {
   banner1: require('~/assets/images/banner1.png'),
   banner2: require('~/assets/images/banner2.png'),
   banner3: require('~/assets/images/banner3.png'),
+  arrival1: require('~/assets/images/arrivals/nike_kyrie_low4.png'),
+  arrival2: require('~/assets/images/arrivals/nike_pg5.png'),
+  arrival3: require('~/assets/images/arrivals/nike_cosmic_unity.png'),
 };
 
 export default images;
