0e7c5e572245d734214d8b061c90d39d35cb6226
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-09-26 11:02:28 +0700

    config smt

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import mongoose from "mongoose";
 import authRoutes from "./routers/auth.js";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
