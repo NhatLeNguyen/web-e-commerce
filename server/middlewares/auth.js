ae04ef030daaa399771ec9da132719e340c66f94
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-09-26 11:02:28 +0700

    config smt

--- server/middlewares/auth.js
+++
@@ -1,9 +0,0 @@
-import jwt from "jsonwebtoken";
-
-const generateToken = (user) => {
-  return jwt.sign({ id: user._id, email: user.email }, process.env.JWT_SECRET, {
-    expiresIn: "1h", // Token sẽ hết hạn sau 1 giờ
-  });
-};
-
-export default generateToken;
