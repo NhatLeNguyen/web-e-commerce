9dc288630e34cdb5d6651c5e7454795bec8c6585
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-10-11 15:25:04 +0700

    fix deploy 6

--- server/index.js
+++ server/index.js
@@ -39,6 +39,8 @@ app.use(
       }
     },
     credentials: true,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
   })
 );
 
