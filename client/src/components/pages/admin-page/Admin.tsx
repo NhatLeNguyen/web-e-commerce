f3511132b2769b8f66c2d93035a43ab7b8b4b1df
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-10-11 11:17:34 +0700

    upd smt

--- client/src/components/pages/admin-page/Admin.tsx
+++ client/src/components/pages/admin-page/Admin.tsx
@@ -3,11 +3,17 @@ import Box from "@mui/material/Box";
 import AppTheme from "../../themes/auth- themes/AuthTheme";
 import DashboardSidebar from "./sidebar/AdminSidebar";
 import Dashboard from "./dashboard/Dashboard";
+import { CssBaseline } from "@mui/material";
+import ColorModeSelect from "../../themes/auth- themes/ColorModeSelect";
 
 const AdminPage: React.FC = () => {
   return (
     <AppTheme>
+      <CssBaseline enableColorScheme />
       <Box sx={{ display: "flex" }}>
+        <Box sx={{ position: "fixed", top: "1rem", right: "1rem" }}>
+          <ColorModeSelect />
+        </Box>
         <DashboardSidebar />
         <Box component="main" sx={{ flexGrow: 1, padding: 3 }}>
           <Dashboard />
