b4875e7fa84c2775f0c7bc6427e647676e849be8
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-10-11 14:03:26 +0700

    fix deploy 1

--- client/src/components/pages/home-page/appBar/cart-modal/cartModal.tsx
+++ client/src/components/pages/home-page/appBar/cart-modal/cartModal.tsx
@@ -14,7 +14,7 @@ import {
   fetchCart,
   removeItemFromCart,
 } from "../../../../../redux/cart/cartThunks";
-import "./CartModal.scss";
+import "./cartModal.scss";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
