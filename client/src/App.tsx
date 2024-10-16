748b876ca28ae62bb45780638d7489913e7a4b75
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-10-16 10:25:32 +0700

    fix feedback.js vercel

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,16 +4,9 @@ import AppRoutes from "./routes/Routes";
 import { useDispatch } from "react-redux";
 import { AppDispatch } from "./redux/stores";
 import { restoreUser, User } from "./redux/auth/authSlice";
-import ReactGA from "react-ga4";
-
-const measurementId = "G-X34NWK577Z";
 
 const App: React.FC = () => {
   const dispatch = useDispatch<AppDispatch>();
-  useEffect(() => {
-    ReactGA.initialize(measurementId);
-    ReactGA.send("pageview");
-  }, []);
 
   useEffect(() => {
     const user = localStorage.getItem("user");
