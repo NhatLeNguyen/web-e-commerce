26e4d381693cab2e94ab3d265b0d08a690258cad
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-10-21 17:00:12 +0700

    fix python 1

--- server/generate_response.py
+++ server/generate_response.py
@@ -4,7 +4,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 def generate_response(prompt):
     model_name = "microsoft/DialoGPT-medium"
     model = AutoModelForCausalLM.from_pretrained(model_name)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side='left')
 
     inputs = tokenizer.encode(prompt + tokenizer.eos_token, return_tensors="pt")
     outputs = model.generate(inputs, max_length=150, pad_token_id=tokenizer.eos_token_id)
@@ -12,6 +12,9 @@ def generate_response(prompt):
     return response
 
 if __name__ == "__main__":
-    prompt = sys.argv[1]
-    response = generate_response(prompt)
-    print(response)
+    if len(sys.argv) > 1:
+        prompt = sys.argv[1]
+        response = generate_response(prompt)
+        print(response)
+    else:
+        print("Error: No prompt provided.")
