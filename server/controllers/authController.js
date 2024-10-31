17bb34ef37d50e82bd6aa6bc9d6ec7baffb93910
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-10-31 09:47:57 +0700

    set cookie

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -70,6 +70,7 @@ export const login = async (req, res) => {
       httpOnly: true,
       secure: true,
       sameSite: "None",
+      maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
     res.status(200).json({
