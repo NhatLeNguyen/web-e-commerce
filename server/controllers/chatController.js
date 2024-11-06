ee9c3667788d4e65b9dc301e76f40c99249d2953
NhatLeNguyen <nhatlenguyen843@gmail.com>
2024-11-06 14:25:21 +0700

    test hugging face api

--- server/controllers/chatController.js
+++ server/controllers/chatController.js
@@ -9,7 +9,7 @@ export const sendMessageToHuggingFace = async (message, context) => {
 
   try {
     const response = await axios.post(
-      "https://api-inference.huggingface.co/models/EleutherAI/gpt-j-6b",
+      "https://api-inference.huggingface.co/models/meta-llama/Meta-Llama-3-8B-Instruct",
       {
         inputs: prompt,
       },
