5a4cba9d19232758631ae176954c733393f7f5d4
NhatLeNguyen <nhatlenguyen843@gmail.com>
2025-02-22 16:19:35 +0700

    fix vnpay payment 9

--- server/routers/paymentRoutes.js
+++ server/routers/paymentRoutes.js
@@ -2,11 +2,13 @@ import express from "express";
 import {
   createVNPayPayment,
   handleVNPayIPN,
-} from "../controllers/orderController.js";
+  handleVNPayReturn,
+} from "../controllers/paymentController.js";
 
 const router = express.Router();
 
 router.post("/create-vnpay-payment", createVNPayPayment);
 router.get("/vnpay-ipn", handleVNPayIPN);
+router.get("/vnpay_return", handleVNPayReturn);
 
 export default router;
