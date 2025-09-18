ec7b4eb67beec25051f4f4a2722f0138f34b3b72
NhatLeNguyen <nhatlenguyen843@gmail.com>
2025-09-18 15:28:11 +0700

    add dockerfile

--- client/src/redux/auth/authThunks.ts
+++ client/src/redux/auth/authThunks.ts
@@ -68,10 +68,7 @@ export const register = createAsyncThunk<
       ...credentials,
       role: "guest",
     });
-    // const { accessToken, user } = response.data;
 
-    // localStorage.setItem("accessToken", accessToken);
-    // localStorage.setItem("user", JSON.stringify(user));
     return response.data;
 
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
